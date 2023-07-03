2f87938637dc40df028921017c3e9e0d3ef65c63
gitofaman <79747232+gitofaman@users.noreply.github.com>
2023-07-04 03:24:19 +0530

    Update updatedTopSlideAnimation.js

--- updatedTopSlideAnimation.js
+++ updatedTopSlideAnimation.js
@@ -1,18 +1,11 @@
 // ALSO NEED TO ADD - <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js" integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 //Alternate Animations script
-var animatedParents = Array.from(document.querySelectorAll('[anime-parent]'))
 var moveHeight = 100;
-var easingType = 'easeOutSine'
-var slideUpDuration = 400
-
-animatedParents.forEach(animeParent=>{
-    //assigning anime child to all it's child
-    if(!!animeParent.querySelector('[anime-child]')) {
-        //it makes sure you can add custom child if you want
-        return;
-    }
-    if(animeParent) {
-        animeParent.childNodes.forEach(childNode=>{
+var moveDirection = 'up' //up, down, left, right, none (it means it'll just fade out)
+var animatedParents = Array.from(document.querySelectorAll('[anime-parent]'))
+animatedParents.forEach(animeParent => {
+    if (animeParent) {
+        animeParent.childNodes.forEach(childNode => {
             childNode.setAttribute('anime-child', 'true')
         })
     }
@@ -21,46 +14,85 @@ animatedParents.forEach(animeParent=>{
 
 
 var alternateAnimations = arr => {
-    //animating elements in array line by line
-    for(i=0;i<arr.length;i++) {
-        slideUpAnimate(arr[i], slideUpDuration, (slideUpDuration*(i+1))/2, 0, 1)
+    duration = 400
+    for (i = 0; i < arr.length; i++) {
+        slideUpAnimate(arr[i], duration, (duration * (i + 1)) / 2, 0, 1)
     }
 }
 
-var slideUpAnimate = (el, duration, delay, yTranlate, opacity) => {
-    setTimeout(()=>{
-        anime({
-            targets: el,
-            translateY: yTranlate,
-            opacity: opacity,
-            duration: duration,
-            easing: easingType
-          });
-    }, delay)
+var slideUpAnimate = (el, duration, delay, disTranlate, opacity) => {
+    switch (moveDirection) {
+        case 'up':
+            setTimeout(() => {
+                anime({
+                    targets: el,
+                    translateY: disTranlate,
+                    opacity: opacity,
+                    duration: duration,
+                    easing: 'easeOutSine'
+                });
+            }, delay)
+            break;
+        case 'down':
+            setTimeout(() => {
+                anime({
+                    targets: el,
+                    translateY: -disTranlate,
+                    opacity: opacity,
+                    duration: duration,
+                    easing: 'easeOutSine'
+                });
+            }, delay)
+            break;
+        case 'left':
+            setTimeout(() => {
+                anime({
+                    targets: el,
+                    translateX: -disTranlate,
+                    opacity: opacity,
+                    duration: duration,
+                    easing: 'easeOutSine'
+                });
+            }, delay)
+            break;
+        case 'right':
+            setTimeout(() => {
+                anime({
+                    targets: el,
+                    translateX: disTranlate,
+                    opacity: opacity,
+                    duration: duration,
+                    easing: 'easeOutSine'
+                });
+            }, delay)
+            break;
+        case 'none':
+            setTimeout(() => {
+                anime({
+                    targets: el,
+                    opacity: opacity,
+                    duration: duration,
+                    easing: 'easeOutSine'
+                });
+            }, delay)
+            break;
+    }
 }
 
-var isElementInView = (t) => {
-    var topPos = t.getBoundingClientRect().top
-    var tHeight = t.offsetHeight;
-    if(tHeight> 600) {
-        tHeight = 400;
+var isElementInView = (el) => {
+    var percentVal = 80;
+    var windowHeight = window.innerHeight
+    var spaceToCover = Math.round(windowHeight * percentVal / 100)
+    var spaceFromEachSides = (windowHeight - spaceToCover) / 2;
+    var elTop = el.getBoundingClientRect().top
+    // var elBottom = el.getBoundingClientRect().bottom
+    if (elTop + el.offsetHeight > spaceFromEachSides && elTop < windowHeight - spaceFromEachSides) {
+        return true;
     }
-    if(tHeight<100) {
-        tHeight = 100;
-    }
-    var offsetPos = 0.5;
-    var offsetAttribute = t.getAttribute('offset-pos')
-    if(!!offsetAttribute) {
-        offsetPos = parseFloat(offsetAttribute)
-    }
-    var isTargetPosReached = false;
-    var topMaxPos = window.innerHeight - Math.round(offsetPos * t.offsetHeight)
-    var topMinPos = - Math.round(offsetPos*t.offsetHeight)
-    var isTargetPosReached = (topPos < topMaxPos) && (topPos > topMinPos)
-    return isTargetPosReached;
+    return false
 }
 
-animatedParents.forEach(parent=>{
+animatedParents.forEach(parent => {
     var animatedChilds = parent.querySelectorAll('[anime-child=true]')
     animatedChilds.forEach(child => {
         slideUpAnimate(child, 0, 0, moveHeight, 0)
@@ -68,8 +100,8 @@ animatedParents.forEach(parent=>{
 })
 
 function animateIfParentInView() {
-    animatedParents.forEach(parentEl=>{
-        if(isElementInView(parentEl)) {
+    animatedParents.forEach(parentEl => {
+        if (isElementInView(parentEl)) {
             alternateAnimations(parentEl.querySelectorAll('[anime-child=true]'))
             var thisParentIndex = animatedParents.indexOf(parentEl)
             animatedParents.splice(thisParentIndex, 1)
@@ -78,4 +110,5 @@ function animateIfParentInView() {
 }
 
 window.addEventListener('scroll', animateIfParentInView)
+animateIfParentInView()
 animateIfParentInView()
